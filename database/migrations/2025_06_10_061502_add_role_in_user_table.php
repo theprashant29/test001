90e70a6fd5384068f5acaef4d532c014341a6615
Prashant <prashant.k8851@gmail.com>
2025-06-12 11:43:19 +0530

    Update 2025_06_10_061502_add_role_in_user_table.php
    add a default admin role user

--- database/migrations/2025_06_10_061502_add_role_in_user_table.php
+++ database/migrations/2025_06_10_061502_add_role_in_user_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -14,6 +16,18 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('role')->default('user')->after('email');
         });
+
+        //add a admin
+         if (DB::table('users')->where('email', 'admin@example.com')->doesntExist()) {
+            DB::table('users')->insert([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'role' => 'admin',
+                'password' => Hash::make('password123'), // Use a strong password
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 
     /**
